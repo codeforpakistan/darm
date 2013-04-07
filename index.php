575b7ce7a0a3e7c331bca68a7c3522c775b67251
Sawant Shah <sawant@sawantshah.com>
2013-04-07 14:28:03 +0500

    Printing username/password.

--- index.php
+++ index.php
@@ -2,5 +2,20 @@
 /**
  *  Disaster and Relief Management System
  */
+    $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"],1);
+
+    mysql_connect($server, $username, $password);
+            
+    
+    mysql_select_db($db);
 ?>
 <h1>Disaster and Relief Management System</h1>
+
+<?php
+	echo "Username: " . $username . " Password: " . $password;
+?>
