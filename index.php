5426bd7ca99147c637762f7c3d2f532118070e05
Sawant Shah <sawant@sawantshah.com>
2013-04-07 14:53:20 +0500

    Added Drupal with Bootstrap theme.

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <?php
+
 /**
- *  Disaster and Relief Management System
+ * @file
+ * The PHP page that serves all page requests on a Drupal installation.
+ *
+ * The routines here dispatch control to the appropriate handler, which then
+ * prints the appropriate page.
+ *
+ * All Drupal code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt.
  */
-    $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-    $server = $url["host"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $db = substr($url["path"],1);
+/**
+ * Root directory of Drupal installation.
+ */
+define('DRUPAL_ROOT', getcwd());
 
-    mysql_connect($server, $username, $password);
-            
-    
-    mysql_select_db($db);
-?>
-<h1>Disaster and Relief Management System</h1>
+require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+menu_execute_active_handler();
